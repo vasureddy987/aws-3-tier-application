b9a1f83bb3360ca7b1b3126c4fac16400708cd92
Vasu Reddy <vasureddy@example.com>
2025-01-06 21:50:29 +0530

    aws 3-tier architecture

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
-    DB_DATABASE : 'webappdb'
+    DB_HOST : '',
+    DB_USER : '',
+    DB_PWD : '',
+    DB_DATABASE : ''
 });
